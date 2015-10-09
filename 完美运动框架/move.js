24170d457b231e87cecc13195c3f43d8c318bd00
carrieguo <593774767@qq.com>
2015-10-09 23:30:56 +0800

    Finished!
    用json数组实现多个值同时变换；为了解多个值改变定时器冲突的问题，设置一个值oStop,
    判断json里面所有的值是否到达了目标值，所有值到达目标点之后才关闭定时器。

--- 完美运动框架/move.js
+++ 完美运动框架/move.js
@@ -14,34 +14,32 @@ function getStyle(obj,name)   //获取非行间样式 currentStyle兼容IE; getComputedSt
         return getComputedStyle(obj, false)[name];
     }
 }
-function startMove(obj,item,iTarget,fnEnd)
-{
 
+function startMove(obj,json,fnEnd)
+{
     //document.title=cur;
     clearInterval(obj.timer);
     obj.timer = setInterval(function(){
-        var cur = 0;
-        if(item=='opacity')
+        var oStop = true;   //在每次json循环之前假设所有值都到达目标点
+        for(var item in json)
         {
-            cur = Math.round(parseFloat(getStyle(obj,item))*100);  //四舍五入去掉小数。
-        }
-        else
-        {
-            cur= parseInt(getStyle(obj,item));
-        }
-        var speed = (iTarget-cur)/8;
-        speed = speed>0?Math.ceil(speed):Math.floor(speed);
-        if(iTarget==cur)
-        {
-            clearInterval(obj.timer);
-
-            if(fnEnd)
+            var cur = 0;
+            if(item=='opacity')
             {
-                fnEnd();
+                cur = Math.round(parseFloat(getStyle(obj,item))*100);  //四舍五入去掉小数。
             }
-        }
-        else
-        {
+            else
+            {
+                cur= parseInt(getStyle(obj,item));
+            }
+            var speed = (json[item]-cur)/70;
+            speed = speed>0?Math.ceil(speed):Math.floor(speed);
+
+            if(json[item]!=cur)   // 判断json循环时如果有值没达到目标点，不停止定时器
+            {
+                oStop = false;
+            }
+
             if(item=='opacity')
             {
                 obj.style.filter = "alpha(opacity:"+cur+speed+")";
@@ -51,8 +49,19 @@ function startMove(obj,item,iTarget,fnEnd)
             {
                 obj.style[item]= cur+speed+'px';
             }
+
+
+            document.title=json[item]+"+"+cur;
         }
-        document.title=iTarget+"+"+cur;
+
+        if(oStop==true)
+        {
+            clearInterval(obj.timer);
+            if(fnEnd)
+            {
+                fnEnd();
+            }
+        }
     },30);
 }/**
  * Created by Administrator on 2015/10/7.
